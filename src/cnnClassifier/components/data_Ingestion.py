f224fdad4c4cd01b8788ee880571ae2888fc936f
Prasmu007 <prasmit000@gmail.com>
2023-08-30 16:46:55 +0530

    normal update

--- src/cnnClassifier/components/data_Ingestion.py
+++ src/cnnClassifier/components/data_Ingestion.py
@@ -5,6 +5,7 @@ from cnnClassifier import logger
 from cnnClassifier.utils.common import get_size
 from cnnClassifier.entity.config_entity import DataIngestionConfig
 from pathlib import Path
+
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
